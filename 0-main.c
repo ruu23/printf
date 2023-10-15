02c69b8a7b907c041dda17d8219b750bdc93d273
ruu23 <arwamah777@gmail.com>
2023-10-15 05:58:29 -0700

    update

---
+++ 0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int main()
+{
+	printf("standard output\n");
+	printf("Hi fam !\n");
+
+	printf("__________________\n");
+
+	printf("local output\n");
+	_printf("Hi fam !\n");
+
+	return (0);
+}
