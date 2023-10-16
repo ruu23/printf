ca70722b6c691cac056d8a798b52badce4800d9d
HabibaaNader <habiba.n.shabana@gmail.com>
2023-10-16 08:50:34 -0700

    update

--- printInt.c
+++ printInt.c
@@ -8,29 +8,39 @@ int _print_int(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
-        va_start(args, format);
-	while (*format){
-              if (*format != '%'){
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
 			write(1, format, 1);
-			count++; }
-		else{
+			count++;
+		}
+		else
+		{
 			format++;
-			if (*format == 'd' || *format == 'i'){
-   				 int num = va_arg(args, int);
-   				 char buffer[12];
-  			         int length = 0;
-				 if (num < 0){
-       				    write(1, "-", 1);
-      				    count++;
-			            num = -num;}
-   				 do {
-       				      buffer[length++] = '0' + (num % 10);
-      				      num /= 10;
-				    } while (num > 0);
-				      while (length > 0){
-       				            write(1, &buffer[--length], 1);
-       					    count++;    }
-					     }
+			if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(args, int);
+				char buffer[12];
+				int length = 0;
+
+				if (num < 0)
+				{
+					write(1, "-", 1);
+					count++;
+					num = -num;
+				}
+				do {
+					buffer[length++] = '0' + (num % 10);
+					num /= 10;
+				} while (num > 0);
+				while (length > 0)
+				{
+						write(1, &buffer[--length], 1);
+						count++;
+				}
 		}
 		format++;
 	}
