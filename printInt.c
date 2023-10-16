7a4924927bbb3ca84ef2c4ea2d1bf53e58784103
HabibaaNader <habiba.n.shabana@gmail.com>
2023-10-16 08:10:41 -0700

    updatee

---
+++ printInt.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _print_int - function to print d,i specifiers.
+ * @format: argument
+ * Return: count
+ */
+int _print_int(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+        va_start(args, format);
+	while (*format){
+              if (*format != '%'){
+			write(1, format, 1);
+			count++; }
+		else{
+			format++;
+			if (*format == 'd' || *format == 'i'){
+   				 int num = va_arg(args, int);
+   				 char buffer[12];
+  			         int length = 0;
+				 if (num < 0){
+       				    write(1, "-", 1);
+      				    count++;
+			            num = -num;}
+   				 do {
+       				      buffer[length++] = '0' + (num % 10);
+      				      num /= 10;
+				    } while (num > 0);
+				      while (length > 0){
+       				            write(1, &buffer[--length], 1);
+       					    count++;    }
+					     }
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
