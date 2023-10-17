f9473f2bd65a50863c20b7846537e678ceb2df99
ruu23 <arwamah777@gmail.com>
2023-10-16 23:59:29 -0700

    first

--- _printf.c
+++ _printf.c
@@ -8,41 +8,21 @@
  */
 int _printf(const char *format, ...)
 {
-	char c;
-	const char *s;
-	va_list args;
 	int count = 0;
+	va_list args;
 
+	if (format == NULL)
+	{
+		return (-1);
+	}
 	va_start(args, format);
 	while (*format)
 	{
 		if (*format != '%')
 		{
 			write(1, format, 1);
-			count++;
-		}
-		else
-		{
-			format++;
-			if (*format == 'c')
-			{
-				c = va_arg(args, int);
-				write(1, &c, 1);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				s = va_arg(args, char *);
-				count += write(1, s, strlen(s));
-			}
-			else if (*format == '%')
-			{
-				write(1, "%", 1);
-				count++;
-			}
 		}
 		format++;
 	}
-	va_end(args);
 	return (count);
 }
